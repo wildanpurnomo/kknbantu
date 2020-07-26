51c4aae0514db5f5de5ab4bd349667c79c53ecdb
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-07-27 06:39:23 +0700

    using image in remote, delete local images

--- app/src/main/java/com/kkndesasendang/sendangsmartlearning/ui/learn/audioVisual/AudioVisualChildFragment.java
+++ app/src/main/java/com/kkndesasendang/sendangsmartlearning/ui/learn/audioVisual/AudioVisualChildFragment.java
@@ -1,31 +1,36 @@
 package com.kkndesasendang.sendangsmartlearning.ui.learn.audioVisual;
 
 import android.content.res.AssetFileDescriptor;
+import android.graphics.drawable.Drawable;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.kkndesasendang.sendangsmartlearning.R;
 
 import java.io.IOException;
 
 public class AudioVisualChildFragment extends Fragment {
     public static final String EXTRA_IMAGE = "extra_image";
-    public static final String EXTRA_AUDIO= "extra_audio";
+    public static final String EXTRA_AUDIO = "extra_audio";
 
     private MediaPlayer mMediaPlayer = null;
     private boolean isReady;
@@ -64,7 +69,31 @@ public class AudioVisualChildFragment extends Fragment {
             mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         }
 
-        Glide.with(this).load(getResources().getIdentifier(imageResourceName, "drawable", requireActivity().getPackageName())).into(img);
+        CircularProgressDrawable mCircularProgressDrawable = new CircularProgressDrawable(requireContext());
+        mCircularProgressDrawable.setStrokeWidth(5f);
+        mCircularProgressDrawable.setCenterRadius(30f);
+        mCircularProgressDrawable.start();
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(mCircularProgressDrawable);
+        requestOptions.error(R.drawable.ic_baseline_error_128);
+        requestOptions.timeout(60000);
+        Glide.with(this)
+                .load("http://sicenikapi-dot-sendang-digital-map.et.r.appspot.com/api/material/upload/" + imageResourceName)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        Toast.makeText(requireContext(), "Mohon cek kembali koneksi internet.", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .apply(requestOptions)
+                .into(img);
 
         AssetFileDescriptor afd = requireActivity().getApplicationContext().getResources().openRawResourceFd(audioResourceName);
         try {
